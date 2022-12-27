62f7a9f0b7e53925f811a20d7ae58d0aff8c6a1a
Kamary <gillespiekamary@gmail.com>
2022-12-27 11:37:47 -0700

    added pages to appjs

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
+import Header from "./components/Header";
 import Navbar from "./components/Navbar";
+import AllTeams from "./pages/AllTeams";
+import Blog from "./pages/Blog";
+import Dashboard from "./pages/Dashboard";
+import Landing from "./pages/Landing";
+import Project from "./pages/Project";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Header />
+      <AllTeams />
+      <Blog />
+      <Dashboard />
+      <Landing />
+      <Project />
     </div>
   );
 }
